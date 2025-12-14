1c89406d97cfedf58a686e130b190e5f7064b813
Tezzy-4202 <arun.t3ja@gmail.com>
2025-12-14 11:43:17 +0530

    v1 A simple image imageViewer

--- IV.c
+++ IV.c
@@ -8,10 +8,22 @@ int main(int argc, char *argv[])
     SDL_Init(SDL_INIT_VIDEO);
 
     printf("Hello World! \n");
-    SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", 900, 600, 0);
-    SDL_Delay(3000);
+    SDL_Surface *pImage = SDL_LoadBMP("test.bmp");
 
-    SDL_DestroyWindow(pwindow);
+    if (!pImage)
+    {
+        SDL_Log("Failed to load image: %s", SDL_GetError());
+        return 1;
+    }
+    SDL_Window *pWindow = SDL_CreateWindow("Image Viewer", pImage->w, pImage->h, 0);
+    SDL_Surface *pSurface = SDL_GetWindowSurface(pWindow);
+
+    SDL_BlitSurface(pImage, NULL, pSurface, NULL);
+    SDL_UpdateWindowSurface(pWindow);
+    SDL_Delay(5000);
+
+    SDL_DestroySurface(pImage);
+    SDL_DestroyWindow(pWindow);
     SDL_Quit();
     return 0;
 }
