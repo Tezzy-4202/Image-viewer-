ac4f83f6c23e18f5a24f38d72b2a4c4d3ab281e3
Tezzy-4202 <arun.t3ja@gmail.com>
2025-12-14 11:52:25 +0530

    v1.1 Added window manipulation

--- IV.c
+++ IV.c
@@ -5,22 +5,62 @@
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
-
-    printf("Hello World! \n");
-    SDL_Surface *pImage = SDL_LoadBMP("test.bmp");
-
-    if (!pImage)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        SDL_Log("Failed to load image: %s", SDL_GetError());
         return 1;
     }
-    SDL_Window *pWindow = SDL_CreateWindow("Image Viewer", pImage->w, pImage->h, 0);
-    SDL_Surface *pSurface = SDL_GetWindowSurface(pWindow);
 
-    SDL_BlitSurface(pImage, NULL, pSurface, NULL);
+    SDL_Surface *pImage = SDL_LoadBMP("test.bmp");
+    if (!pImage)
+    {
+        return 1;
+    }
+
+    int maxWidth = 1000;
+    int maxHeight = 800;
+    int finalW = pImage->w;
+    int finalH = pImage->h;
+
+    if (finalW > maxWidth)
+    {
+        float scale = (float)maxWidth / finalW;
+        finalW = maxWidth;
+        finalH = (int)(finalH * scale);
+    }
+    if (finalH > maxHeight)
+    {
+        float scale = (float)maxHeight / finalH;
+        finalH = maxHeight;
+        finalW = (int)(finalW * scale);
+    }
+
+    SDL_Window *pWindow = SDL_CreateWindow("Vibe Manipulator", finalW, finalH, 0);
+    if (!pWindow)
+    {
+        SDL_DestroySurface(pImage);
+        return 1;
+    }
+
+    SDL_Surface *pWindowSurface = SDL_GetWindowSurface(pWindow);
+
+    SDL_Rect destRect = {0, 0, finalW, finalH};
+    SDL_BlitSurfaceScaled(pImage, NULL, pWindowSurface, &destRect, SDL_SCALEMODE_LINEAR);
     SDL_UpdateWindowSurface(pWindow);
-    SDL_Delay(5000);
+
+    int running = 1;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = 0;
+            }
+        }
+        SDL_Delay(10);
+    }
 
     SDL_DestroySurface(pImage);
     SDL_DestroyWindow(pWindow);
